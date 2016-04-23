5ad4d3bb98f39d12e38596af2a202258afdb2ff6
John Mercouris <jmercouris@gmail.com>
2016-04-23 15:11:29 +0200

    SQLAlchemy initialization code in place

--- source/initialize.py
+++ source/initialize.py
@@ -14,7 +14,7 @@ class User(Base):
     symbol = Column(Text)
 
 
-def create_database(database_name):
+def create_database(database_name='sqlite:///data/data_store.db'):
     # Create an engine that stores data in the local directory's
     # 'sqlite:///data/data_store.db' file.
     engine = create_engine(database_name)
@@ -22,6 +22,10 @@ def create_database(database_name):
     # Create all tables in the engine. This is equivalent to "Create Table"
     # statements in raw SQL.
     Base.metadata.create_all(engine)
+    print('Database {} Initialized'.format(database_name))
 
-    
+
+if __name__ == "__main__":
+    create_database()
+
 
