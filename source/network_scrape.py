2b03ab1d25a8deef5176afd096c595c092f905d5
John Mercouris <jmercouris@gmail.com>
2016-04-23 15:07:04 +0200

    Sqlite persistence of gathered data

--- source/network_scrape.py
+++ source/network_scrape.py
@@ -1,5 +1,6 @@
 from twython import Twython
 import configparser
+import initialize
 
 settings = configparser.ConfigParser()
 settings.read('configuration.ini')
@@ -8,12 +9,14 @@ APP_KEY = settings.get('twython-configuration', 'key')
 APP_SECRET = settings.get('twython-configuration', 'secret')
 OAUTH_TOKEN = settings.get('twython-configuration', 'token')
 OAUTH_TOKEN_SECRET = settings.get('twython-configuration', 'token_secret')
+DATABASE_NAME = settings.get('database-configuration', 'database_name')
 
 
 def main():
-    twitter = Twython(APP_KEY, APP_SECRET,
-                      OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-    print(twitter.get_home_timeline())
+    # twitter = Twython(APP_KEY, APP_SECRET,
+    #                   OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    # print(twitter.get_home_timeline())
+    initialize.create_database(DATABASE_NAME)
 
 
 if __name__ == "__main__":
