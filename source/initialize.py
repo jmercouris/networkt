17aa73214a42b3e8fa5f77bb4436c76f6146a99e
John Mercouris <john@mercouris.email>
2016-04-23 15:07:04 +0200

    Sqlite persistence of gathered data

---
+++ source/initialize.py
@@ -0,0 +1,27 @@
+""" Initialize the database tables, columns, etc
+"""
+from sqlalchemy import Column, Integer, Float, Date, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    symbol = Column(Text)
+
+
+def create_database(database_name):
+    # Create an engine that stores data in the local directory's
+    # 'sqlite:///data/data_store.db' file.
+    engine = create_engine(database_name)
+ 
+    # Create all tables in the engine. This is equivalent to "Create Table"
+    # statements in raw SQL.
+    Base.metadata.create_all(engine)
+
+    
+
