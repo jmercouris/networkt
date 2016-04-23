1a3a1aab76ab4777bd73bd00c1724f5e1434ed21
John Mercouris <john@mercouris.email>
2016-04-23 13:14:13 +0200

    Twython setup complete

--- source/network_scrape.py
+++ source/network_scrape.py
@@ -1,11 +1,20 @@
+from twython import Twython
 import configparser
 
 settings = configparser.ConfigParser()
 settings.read('configuration.ini')
 
-key = settings.get('twython-configuration', 'key')
-secret = settings.get('twython-configuration', 'secret')
-token = settings.get('twython-configuration', 'token')
-token_secret = settings.get('twython-configuration', 'token_secret')
+APP_KEY = settings.get('twython-configuration', 'key')
+APP_SECRET = settings.get('twython-configuration', 'secret')
+OAUTH_TOKEN = settings.get('twython-configuration', 'token')
+OAUTH_TOKEN_SECRET = settings.get('twython-configuration', 'token_secret')
 
 
+def main():
+    twitter = Twython(APP_KEY, APP_SECRET,
+                      OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    print(twitter.get_home_timeline())
+
+
+if __name__ == "__main__":
+    main()
