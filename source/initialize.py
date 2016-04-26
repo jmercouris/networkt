3d1cba2a553bdf3149769de9187bc8d05051c6e0
John Mercouris <john@mercouris.email>
2016-04-26 22:52:39 +0200

    Developed model for directed graph in sqlalchemy

--- source/initialize.py
+++ source/initialize.py
@@ -2,15 +2,17 @@
 """
 from sqlalchemy import Column, Text, Boolean, Integer
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-import pprint
+from sqlalchemy.orm import mapper, relationship, sessionmaker
+from sqlalchemy import MetaData, Table, Column, Integer, ForeignKey, create_engine
+
 
 Base = declarative_base()
 
 
-class User(Base):
-    __tablename__ = 'users'
+class Node(Base):
+    __tablename__ = 'node'
     id = Column(Integer, primary_key=True)
+    node_id = Column(Integer, primary_key=True)
     created_at = Column(Text)
     description = Column(Text)
     favorites_count = Column(Integer)
@@ -26,9 +28,8 @@ class User(Base):
     time_zone = Column(Text)
     utc_offset = Column(Integer)
     verified = Column(Boolean)
-    
+
     def __init__(self, dictionary):
-        pprint.pprint(dictionary)
         self.created_at = dictionary.get('created_at', None)
         self.description = dictionary.get('description', None)
         self.favorites_count = int(dictionary.get('favourites_count') or -1)
@@ -44,13 +45,53 @@ class User(Base):
         self.time_zone = dictionary.get('time_zone', None)
         self.utc_offset = int(dictionary.get('utc_offset') or -1)
         self.verified = bool(dictionary.get('verified', False) or False)
+        self.node_id = int(self.id_str)
+
+    def add_neighbors(self, *nodes):
+        for node in nodes:
+            Edge(self, node)
+        return self
+
+    def higher_neighbors(self):
+        return [x.higher_node for x in self.lower_edges]
+
+    def lower_neighbors(self):
+        return [x.lower_node for x in self.higher_edges]
+
+
+class Edge(Base):
+    __tablename__ = 'edge'
+
+    lower_id = Column(Integer,
+                      ForeignKey('node.node_id'),
+                      primary_key=True)
+
+    higher_id = Column(Integer,
+                       ForeignKey('node.node_id'),
+                       primary_key=True)
+
+    lower_node = relationship(Node,
+                              primaryjoin=lower_id == Node.node_id,
+                              backref='lower_edges')
+    higher_node = relationship(Node,
+                               primaryjoin=higher_id == Node.node_id,
+                               backref='higher_edges')
+
+    # here we have lower.node_id <= higher.node_id
+    def __init__(self, n1, n2):
+        if n1.node_id < n2.node_id:
+            self.lower_node = n1
+            self.higher_node = n2
+        else:
+            self.lower_node = n2
+            self.higher_node = n1
 
 
 def create_database(database_name='sqlite:///data/data_store.db'):
     # Create an engine that stores data in the local directory's
     # 'sqlite:///data/data_store.db' file.
     engine = create_engine(database_name)
- 
+
     # Create all tables in the engine. This is equivalent to "Create Table"
     # statements in raw SQL.
     Base.metadata.create_all(engine)
@@ -59,5 +100,3 @@ def create_database(database_name='sqlite:///data/data_store.db'):
 
 if __name__ == "__main__":
     create_database()
-
-
