142f7e57a54d712967587bd74ca2a87fc8761099
John Mercouris <john@mercouris.email>
2016-05-01 19:15:56 +0200

    Updated gitignore

--- source/network_scrape.py
+++ source/network_scrape.py
@@ -29,15 +29,22 @@ session = DBSession()
 # Main Function
 def main(root_user='FactoryBerlin'):
     persist_user(root_user)
+    # pull_remote_graph(root_user)
+    # Show All Objects Referencing root_user
 
 
 def persist_user(screen_name):
-    instance = Node(twitter.lookup_user(screen_name=screen_name)[0])
-    session.add(instance)
-    session.commit()
+    root_user_object = session.query(Node).filter_by(screen_name=screen_name).first()
+    if (root_user_object is None):
+        instance = Node(twitter.lookup_user(screen_name=screen_name)[0])
+        session.add(instance)
+        session.commit()
 
 
-def pull_remote_graph(screen_name, scope_limit=100):
+def pull_remote_graph(screen_name, scope_limit=1):
+    # Collect the Root Object
+    root_user_object = session.query(Node).filter_by(screen_name=screen_name).first()
+
     next_cursor = -1
     while(next_cursor and scope_limit):
         # Limit how many Total Friends we get data for
@@ -50,6 +57,8 @@ def pull_remote_graph(screen_name, scope_limit=100):
             # args = {key: value for key, value in results.items() if key in User.__mapper__.attrs}
             instance = Node(result)
             session.add(instance)
+            # Append Relationships
+            instance.add_edge(root_user_object)
 
         # Get Next Cursor, Commit, and Sleep for next iteration
         next_cursor = search["next_cursor"]
