9978806b5686fbc1990e43b3b135fa640b6abe24
John Mercouris <john@mercouris.email>
2016-05-03 14:23:40 +0200

    Save

--- source/filter_node.py
+++ source/filter_node.py
@@ -1,17 +1,20 @@
+import numpy as np
+
 """ Used for filtering nodes as to whether they should be expanded or not """
 
 
 # First Level of Filtering, used to determine whether first degree of nodes will be retrieved
 def filter_0(node):
-    return logical_time_zone(node)
+    return (logical_time_zone(node) and logical_name(node))
 
 
 # Second Degree of Filtering, used to determine whether second degree of nodes will be retrieved
 def filter_1(node):
-    return False
+    return logical_distribution(node)
 
 
-def logical_distribution():
+def logical_distribution(node):
+    print([n.screen_name for n in node.reference_nodes()])
     return False
 
 
@@ -23,3 +26,6 @@ def logical_time_zone(node):
     else:
         return False
 
+
+def logical_name(node):
+    return False
--- source/network_scrape.py
+++ source/network_scrape.py
@@ -34,21 +34,22 @@ def main(root_user='FactoryBerlin'):
 
     ##########################################################################
     # Pull the Graph for the Root User
-    pull_remote_graph(root_user)
+    pull_remote_graph_follow(root_user)
     
     ##########################################################################
     # Perform level 0 filtering on user - determine if their 1th degree network
     # is something that should be retrieved
     root_user_object = session.query(Node).filter_by(screen_name=root_user).first()
-    for node in root_user_object.reference_nodes():
+    for node in root_user_object.pointer_nodes():
         node.filter_0 = filter_node.filter_0(node)
     session.commit()
     
     ##########################################################################
     # Pull partial graphs of all users following root users
-    for node in root_user_object.reference_nodes():
-        pull_remote_graph(node.screen_name)
-        graph.persist_graph(node.screen_name, node.screen_name)
+    for node in root_user_object.pointer_nodes():
+        if (node.filter_0):
+            pull_remote_graph_friend(node.screen_name)
+            graph.persist_graph(node.screen_name, node.screen_name)
     
     ##########################################################################
     # Perform level 1 filtering on user - determine if their 1th degree network
@@ -60,17 +61,16 @@ def main(root_user='FactoryBerlin'):
 
 
 def persist_user(screen_name):
-    root_user_object = session.query(Node).filter_by(
-        screen_name=screen_name).first()
-    if (root_user_object is None):
+    user_object = session.query(Node).filter_by(screen_name=screen_name).first()
+    if (user_object is None):
         instance = Node(twitter.lookup_user(screen_name=screen_name)[0])
         session.add(instance)
         session.commit()
 
 
-def pull_remote_graph(screen_name, scope_limit=1):
-    root_user_object = session.query(Node).filter_by(screen_name=screen_name).first()
-    if (root_user_object.reference_nodes() is not None or root_user_object.pointer_nodes is not None):
+def pull_remote_graph_friend(screen_name, scope_limit=1):
+    user_object = session.query(Node).filter_by(screen_name=screen_name).first()
+    if (len(user_object.pointer_nodes()) > 5):
         return
     next_cursor = -1
     while(next_cursor and scope_limit):
@@ -86,7 +86,33 @@ def pull_remote_graph(screen_name, scope_limit=1):
                 instance = Node(result)
                 session.add(instance)
                 # Append Relationships
-                instance.add_edge(root_user_object)
+            instance.add_edge(user_object)
+        
+        # Get Next Cursor, Commit, and Sleep for next iteration
+        next_cursor = search["next_cursor"]
+        session.commit()
+        time.sleep(65)
+
+
+def pull_remote_graph_follow(screen_name, scope_limit=1):
+    user_object = session.query(Node).filter_by(screen_name=screen_name).first()
+    if (len(user_object.pointer_nodes()) > 5):
+        return
+    next_cursor = -1
+    while(next_cursor and scope_limit):
+        # Limit how many Total Friends we get data for
+        scope_limit -= 1
+        
+        # Process the next Cursor set of Data
+        search = twitter.get_followers_list(screen_name=screen_name,
+                                            count=200, cursor=next_cursor)
+        for result in search['users']:
+            # Check if User Exists
+            if (session.query(Node).filter_by(screen_name=result['screen_name']).first() is None):
+                instance = Node(result)
+                session.add(instance)
+                # Append Relationships
+            user_object.add_edge(instance)
         
         # Get Next Cursor, Commit, and Sleep for next iteration
         next_cursor = search["next_cursor"]
