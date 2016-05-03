3f4da78c3ce1fa8cc39581d2825d21db71690074
John Mercouris <john@mercouris.email>
2016-05-03 19:43:53 +0200

    Added filtering

--- source/filter_node.py
+++ source/filter_node.py
@@ -1,11 +1,9 @@
-import numpy as np
-
 """ Used for filtering nodes as to whether they should be expanded or not """
 
 
 # First Level of Filtering, used to determine whether first degree of nodes will be retrieved
-def filter_0(node):
-    return (logical_time_zone(node) and logical_name(node))
+def filter_0(node, name_list):
+    return (logical_time_zone(node) and logical_name(node, name_list))
 
 
 # Second Degree of Filtering, used to determine whether second degree of nodes will be retrieved
@@ -27,5 +25,15 @@ def logical_time_zone(node):
         return False
 
 
-def logical_name(node):
-    return True
+def logical_name(node, name_list):
+    return any(i in node.name.upper() for i in name_list)
+
+
+def load_name_list_into_memory(location='static/census-derived-all-first.txt'):
+    output_array = []
+    with open(location) as f:
+        content = f.readlines()
+        for line in content:
+            output_array.append(line.split(' ', 1)[0])
+    return output_array
+
--- source/network_scrape.py
+++ source/network_scrape.py
@@ -29,34 +29,36 @@ session = DBSession()
 def main(root_user='FactoryBerlin'):
     ##########################################################################
     # Save the Root User to the Database
-    persist_user(root_user)
+    # persist_user(root_user)
 
     ##########################################################################
     # Pull the Graph for the Root User
-    pull_remote_graph_follow(root_user)
+    # pull_remote_graph_follow(root_user)
     
     ##########################################################################
     # Perform level 0 filtering on user - determine if their 1th degree network
     # is something that should be retrieved
     root_user_object = session.query(Node).filter_by(screen_name=root_user).first()
+    name_list = filter_node.load_name_list_into_memory()  # Load list of valid names
     for node in root_user_object.pointer_nodes():
-        node.filter_0 = filter_node.filter_0(node)
+        node.filter_0 = filter_node.filter_0(node, name_list)
     session.commit()
     
-    # ##########################################################################
-    # # Pull partial graphs of all filtered users following root user
+    ##########################################################################
+    # Pull partial graphs of all filtered users following root user
     for node in root_user_object.pointer_nodes():
         if (node.filter_0):
             pull_remote_graph_friend(node.screen_name)
             graph.persist_graph(node.screen_name, node.screen_name)
     
-    # ##########################################################################
-    # # Perform level 1 filtering on user - determine if their 1th degree network
-    # # is something that should be retrieved
-    # root_user_object = session.query(Node).filter_by(screen_name=root_user).first()
-    # for node in root_user_object.reference_nodes():
-    #     node.filter_1 = filter_node.filter_1(node)
-    # session.commit()
+    ##########################################################################
+    # Perform level 1 filtering on user - determine if their 1th degree network
+    # is something that should be retrieved
+    root_user_object = session.query(Node).filter_by(screen_name=root_user).first()
+    for node in root_user_object.pointer_nodes():
+        if (node.filter_0):
+            node.filter_1 = filter_node.filter_1(node)
+    session.commit()
 
 
 def persist_user(screen_name):
