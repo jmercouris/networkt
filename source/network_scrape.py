2d59d9a069b7287194c09f04a09511b1168ec30f
John Mercouris <jmercouris@gmail.com>
2016-05-09 12:18:32 +0200

    Added depth of scope

--- source/network_scrape.py
+++ source/network_scrape.py
@@ -27,76 +27,86 @@ session = DBSession()
 
 
 def main(root_user='FactoryBerlin'):
-    # ##########################################################################
-    # # Save the Root User to the Database
-    # persist_user(root_user)
-    # print('Root User: {} Persisted.'.format(root_user))
-    
-    # ##########################################################################
-    # # Pull the Graph for the Root User
-    # pull_remote_graph_follow(root_user)
-    # print('Root User: {} follower graph extracted.'.format(root_user))
-    
-    # ##########################################################################
-    # # Perform level 0 filtering on user - determine if their 1th degree network
-    # # is something that should be retrieved
-    # root_user_object = session.query(Node).filter_by(screen_name=root_user).first()
-    # name_list = filter_node.load_name_list_into_memory()  # Load list of valid names
-    # for node in root_user_object.pointer_nodes():
-    #     node.filter_0 = filter_node.filter_0(node, name_list)
-    # session.commit()
-    # print('Root User: {} follower graph filtered. [Filter level 0]'.format(root_user))
-    
-    # ##########################################################################
-    # # Pull partial graphs of all filtered users following root user
-    # root_user_object = session.query(Node).filter_by(screen_name=root_user).first()
-    # for node in root_user_object.pointer_nodes():
-    #     if (node.filter_0):
-    #         pull_remote_graph_friend(node.screen_name)
-    #         graph.persist_graph(node.screen_name, node.screen_name)
-    # print('Root User: {} follower graphs extracted.'.format(root_user))
-    
-    # ##########################################################################
-    # # Perform level 1 filtering on user - determine if their 1th degree network
-    # # is something that should be retrieved
-    # root_user_object = session.query(Node).filter_by(screen_name=root_user).first()
-    # for node in root_user_object.pointer_nodes():
-    #     if (node.filter_0):
-    #         node.filter_1 = filter_node.filter_1(node)
-    # session.commit()
-    # print('Root User: {} follower graphs filtered. [Filter level 1]'.format(root_user))
-    
-    # ##########################################################################
-    # # Pull extended graphs of all filtered users
-    # root_user_object = session.query(Node).filter_by(screen_name=root_user).first()
-    # for root_node in root_user_object.pointer_nodes():
-    #     if (root_node.filter_0 and root_node.filter_1):
-    #         for node in root_node.reference_nodes():
-    #             pull_remote_graph_friend(node.screen_name)
-    #             print(root_node.screen_name, node.screen_name)
-    
+    ##########################################################################
+    # Save the Root User to the Database
+    persist_user(root_user)
+    print('Root User: {} Persisted.'.format(root_user))
+
+    ##########################################################################
+    # Pull the Graph for the Root User
+    pull_remote_graph_follow(root_user)
+    print('Root User: {} follower graph extracted.'.format(root_user))
+
+    ##########################################################################
+    # Perform level 0 filtering on user - determine if their 1th degree network
+    # is something that should be retrieved
+    root_user_object = session.query(Node).filter_by(
+        screen_name=root_user).first()
+    name_list = filter_node.load_name_list_into_memory()  # Load list of valid names
+    for node in root_user_object.pointer_nodes():
+        node.filter_0 = filter_node.filter_0(node, name_list)
+    session.commit()
+    print(
+        'Root User: {} follower graph filtered. [Filter level 0]'.format(root_user))
+
+    ##########################################################################
+    # Pull partial graphs of all filtered users following root user
+    root_user_object = session.query(Node).filter_by(
+        screen_name=root_user).first()
+    for node in root_user_object.pointer_nodes():
+        if (node.filter_0):
+            pull_remote_graph_friend(node.screen_name)
+            graph.persist_graph(node.screen_name, node.screen_name)
+    print('Root User: {} follower graphs extracted.'.format(root_user))
+
+    ##########################################################################
+    # Perform level 1 filtering on user - determine if their 1th degree network
+    # is something that should be retrieved
+    root_user_object = session.query(Node).filter_by(
+        screen_name=root_user).first()
+    for node in root_user_object.pointer_nodes():
+        if (node.filter_0):
+            node.filter_1 = filter_node.filter_1(node)
+    session.commit()
+    print(
+        'Root User: {} follower graphs filtered. [Filter level 1]'.format(root_user))
+
+    ##########################################################################
+    # Pull extended graphs of all filtered users
+    root_user_object = session.query(Node).filter_by(
+        screen_name=root_user).first()
+    for root_node in root_user_object.pointer_nodes():
+        if (root_node.filter_0 and root_node.filter_1):
+            for node in root_node.reference_nodes():
+                pull_remote_graph_friend(node.screen_name)
+                print(root_node.screen_name, node.screen_name)
+
     ##########################################################################
     # Persist graphs of all filtered users
     graph.persist_graph('CookMal', 'CookMal')
 
 
 def persist_user(screen_name):
-    user_object = session.query(Node).filter_by(screen_name=screen_name).first()
+    user_object = session.query(Node).filter_by(
+        screen_name=screen_name).first()
     if (user_object is None):
         instance = Node(twitter.lookup_user(screen_name=screen_name)[0])
         session.add(instance)
         session.commit()
 
 
-def pull_remote_graph(screen_name, scope_limit, twitter_function, edge_check_function, edge_function):
-    user_object = session.query(Node).filter_by(screen_name=screen_name).first()
+def pull_remote_graph(screen_name, scope_limit, scope_depth, twitter_function, edge_check_function, edge_function):
+    user_object = session.query(Node).filter_by(
+        screen_name=screen_name).first()
     next_cursor = -1
-    
+
     while(next_cursor and scope_limit):
         scope_limit -= 1
-        search = twitter_function(screen_name=screen_name, count=200, cursor=next_cursor)
+        search = twitter_function(
+            screen_name=screen_name, count=scope_depth, cursor=next_cursor)
         for result in search['users']:
-            instance = session.query(Node).filter_by(screen_name=result['screen_name']).first()
+            instance = session.query(Node).filter_by(
+                screen_name=result['screen_name']).first()
             if (instance is None):
                 instance = Node(result)
                 session.add(instance)
@@ -121,16 +131,20 @@ def edge_check_reference(instance, user_object):
         return False
 
 
-def pull_remote_graph_friend(screen_name, scope_limit=1):
-    user_object = session.query(Node).filter_by(screen_name=screen_name).first()
+def pull_remote_graph_friend(screen_name, scope_limit=1, scope_depth=200):
+    user_object = session.query(Node).filter_by(
+        screen_name=screen_name).first()
     if (len(user_object.reference_nodes()) < 5):
-        pull_remote_graph(screen_name, scope_limit, twitter.get_friends_list, edge_check_point, edge_point)
+        pull_remote_graph(screen_name, scope_limit, scope_depth,
+                          twitter.get_friends_list, edge_check_point, edge_point)
 
 
-def pull_remote_graph_follow(screen_name, scope_limit=1):
-    user_object = session.query(Node).filter_by(screen_name=screen_name).first()
+def pull_remote_graph_follow(screen_name, scope_limit=1, scope_depth=200):
+    user_object = session.query(Node).filter_by(
+        screen_name=screen_name).first()
     if (len(user_object.pointer_nodes()) < 5):
-        pull_remote_graph(screen_name, scope_limit, twitter.get_followers_list, edge_check_reference, edge_reference)
+        pull_remote_graph(screen_name, scope_limit, scope_depth,
+                          twitter.get_followers_list, edge_check_reference, edge_reference)
 
 
 if __name__ == "__main__":
