553abfd88bd36d3b8ffe0f9c961ecd7448336b1e
John Mercouris <john@mercouris.email>
2016-05-11 12:10:03 +0200

    Graph visualization

--- source/visualize_graph.py
+++ source/visualize_graph.py
@@ -19,26 +19,12 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-# set up fonts
-
-
 # draw the white background onto the surface
 windowSurface.fill(WHITE)
-
-# draw a green polygon onto the surface
-pygame.draw.polygon(windowSurface, GREEN, ((146, 0),
-                                           (291, 106), (236, 277), (56, 277), (0, 106)))
-
-# draw some blue lines onto the surface
-pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
-pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
-pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
 
 # draw a blue circle onto the surface
 pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
 
-# draw a red ellipse onto the surface
-pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
 
 # draw the window onto the screen
 pygame.display.update()
