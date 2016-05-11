1c323de2534fdc0f2489cd5944d371b5e5814ae0
John Mercouris <jmercouris@gmail.com>
2016-05-11 12:13:16 +0200

    Basic network created

--- source/visualize_graph.py
+++ source/visualize_graph.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from pygame.locals import *
 import networkx as nx
+import matplotlib.pyplot as plt
 
 # set up pygame
 pygame.init()
@@ -22,9 +23,16 @@ BLUE = (0, 0, 255)
 # draw the white background onto the surface
 windowSurface.fill(WHITE)
 
+# generate a simple graph
+graph = nx.Graph()
+graph.add_node(1)
+graph.add_node(2)
+graph.add_edge(1, 2)
+nx.draw(graph)
+plt.savefig("path.png")
+
 # draw a blue circle onto the surface
 pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
-
 
 # draw the window onto the screen
 pygame.display.update()
