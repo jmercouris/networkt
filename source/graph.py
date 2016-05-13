976e0973d9cdfdc3debc42f8a5eed31d081179fd
John Mercouris <jmercouris@gmail.com>
2016-05-14 01:15:18 +0200

    Attempting to improve traversal

--- source/graph.py
+++ source/graph.py
@@ -24,7 +24,7 @@ def persist_graph(screen_name, file_name):
     # graph = load_graph_from_database(screen_name)
     graph = nx.DiGraph()
     root_user_object = session.query(Node).filter_by(screen_name=screen_name).first()
-    graph = traverse(root_user_object, 0, {}, graph)
+    graph = traverse(root_user_object, 0, 10, {}, graph)
     nx.write_gml(graph, 'data/graph/' + file_name + '.gml')
 
 
@@ -56,21 +56,23 @@ def load_graph_from_database(screen_name):
     return graph
 
 
-def traverse(node, depth, cache, graph):
+def traverse(node, depth, depth_limit, cache, graph):
     cache[node] = None
     # print('__'*depth + node.name)
+    if (depth > depth_limit):
+        return graph
     graph.add_node(node.screen_name, data=node.construct_dictionary())
     for reference in node.pointer_nodes():
         graph.add_edge(node.screen_name, reference.screen_name)
         if reference in cache:
             continue
-        graph = nx.compose(graph, traverse(reference, depth+1, cache, graph))
+        graph = nx.compose(graph, traverse(reference, depth+1, depth_limit, cache, graph))
 
     for reference in node.reference_nodes():
         graph.add_edge(reference.screen_name, node.screen_name)
         if reference in cache:
             continue
-        graph = nx.compose(graph, traverse(reference, depth+1, cache, graph))
+        graph = nx.compose(graph, traverse(reference, depth+1, depth_limit, cache, graph))
 
     return graph
 
