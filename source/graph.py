fafadc700ed362f1957209e4a78ded2997eef2a0
John Mercouris <jmercouris@gmail.com>
2016-05-14 01:03:31 +0200

    Graph generation success

--- source/graph.py
+++ source/graph.py
@@ -22,7 +22,7 @@ def main(root_user='jmercouris'):
 
 def persist_graph(screen_name, file_name):
     # graph = load_graph_from_database(screen_name)
-    graph = nx.Graph()
+    graph = nx.DiGraph()
     root_user_object = session.query(Node).filter_by(screen_name=screen_name).first()
     graph = traverse(root_user_object, 0, {}, graph)
     nx.write_gml(graph, 'data/graph/' + file_name + '.gml')
@@ -58,24 +58,19 @@ def load_graph_from_database(screen_name):
 
 def traverse(node, depth, cache, graph):
     cache[node] = None
-    print('__'*depth + node.name)
+    # print('__'*depth + node.name)
     graph.add_node(node.screen_name, data=node.construct_dictionary())
-    for ref in node.pointer_nodes():
-        # we are about to recurse, first check if the node we want
-        # to recurse on is in the cache
-        if ref in cache:
-            print('Already in cache: %s' % ref.screen_name)
+    for reference in node.pointer_nodes():
+        graph.add_edge(node.screen_name, reference.screen_name)
+        if reference in cache:
             continue
-        # recurse if we reach this point
-        traverse(ref, depth+1, cache, graph)
-    for ref in node.reference_nodes():
-        # we are about to recurse, first check if the node we want
-        # to recurse on is in the cache
-        if ref in cache:
-            print('Already in cache: %s' % ref.screen_name)
+        graph = nx.compose(graph, traverse(reference, depth+1, cache, graph))
+
+    for reference in node.reference_nodes():
+        graph.add_edge(reference.screen_name, node.screen_name)
+        if reference in cache:
             continue
-        # recurse if we reach this point
-        traverse(ref, depth+1, cache, graph)
+        graph = nx.compose(graph, traverse(reference, depth+1, cache, graph))
 
     return graph
 
