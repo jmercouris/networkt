2b63f4ab8feb5741cf10613126be69d605c83b8d
John Mercouris <jmercouris@gmail.com>
2016-05-13 22:46:26 +0200

    Traversal work

--- source/graph.py
+++ source/graph.py
@@ -17,7 +17,11 @@ session = DBSession()
 
 # Main Function
 def main(root_user='FactoryBerlin'):
-    persist_graph(root_user, root_user)
+    # persist_graph(root_user, root_user)
+    screen_name = 'jmercouris'
+    root_user_object = session.query(Node).filter_by(screen_name=screen_name).first()
+    traverse(root_user_object, 0, {})
+    pass
 
 
 def persist_graph(screen_name, file_name):
@@ -53,6 +57,20 @@ def load_graph_from_database(screen_name):
             graph.add_edge(node.screen_name, nodey.screen_name)
         
     return graph
+
+
+def traverse(node, depth, cache):
+    cache[node] = None
+
+    print('__'*depth + node.name)
+    for ref in node.refs:
+        # we are about to recurse, first check if the node we want
+        # to recurse on is in the cache
+        if ref in cache:
+            print('Already in cache: %s' % ref.name)
+            continue
+        # recurse if we reach this point
+        traverse(ref, depth+1, cache)
 
 
 if __name__ == "__main__":
