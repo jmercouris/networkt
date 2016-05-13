be1b21eeb2a5771b1c2ac66e50b934ea5aac94da
John Mercouris <jmercouris@gmail.com>
2016-05-13 23:40:17 +0200

    Working on traversal

--- source/graph.py
+++ source/graph.py
@@ -16,16 +16,15 @@ session = DBSession()
 
 
 # Main Function
-def main(root_user='FactoryBerlin'):
-    # persist_graph(root_user, root_user)
-    screen_name = 'jmercouris'
-    root_user_object = session.query(Node).filter_by(screen_name=screen_name).first()
-    traverse(root_user_object, 0, {})
-    pass
+def main(root_user='jmercouris'):
+    persist_graph(root_user, root_user)
 
 
 def persist_graph(screen_name, file_name):
-    graph = load_graph_from_database(screen_name)
+    # graph = load_graph_from_database(screen_name)
+    graph = nx.Graph()
+    root_user_object = session.query(Node).filter_by(screen_name=screen_name).first()
+    graph = traverse(root_user_object, 0, {}, graph)
     nx.write_gml(graph, 'data/graph/' + file_name + '.gml')
 
 
@@ -35,7 +34,6 @@ def load_graph_from_database(screen_name):
     graph.add_node(root_user_object.screen_name, data=root_user_object.construct_dictionary())
     
     for node in root_user_object.reference_nodes():
-        print(node.screen_name)
         graph.add_node(node.screen_name, data=node.construct_dictionary())
         graph.add_edge(node.screen_name, root_user_object.screen_name)
         for nodey in node.reference_nodes():
@@ -46,7 +44,6 @@ def load_graph_from_database(screen_name):
             graph.add_edge(node.screen_name, nodey.screen_name)
 
     for node in root_user_object.pointer_nodes():
-        print(node.screen_name)
         graph.add_node(node.screen_name, data=node.construct_dictionary())
         graph.add_edge(root_user_object.screen_name, node.screen_name)
         for nodey in node.reference_nodes():
@@ -59,18 +56,28 @@ def load_graph_from_database(screen_name):
     return graph
 
 
-def traverse(node, depth, cache):
+def traverse(node, depth, cache, graph):
     cache[node] = None
-
     print('__'*depth + node.name)
-    for ref in node.refs:
+    graph.add_node(node.screen_name, data=node.construct_dictionary())
+    for ref in node.pointer_nodes():
         # we are about to recurse, first check if the node we want
         # to recurse on is in the cache
         if ref in cache:
-            print('Already in cache: %s' % ref.name)
+            print('Already in cache: %s' % ref.screen_name)
             continue
         # recurse if we reach this point
-        traverse(ref, depth+1, cache)
+        traverse(ref, depth+1, cache, graph)
+    for ref in node.reference_nodes():
+        # we are about to recurse, first check if the node we want
+        # to recurse on is in the cache
+        if ref in cache:
+            print('Already in cache: %s' % ref.screen_name)
+            continue
+        # recurse if we reach this point
+        traverse(ref, depth+1, cache, graph)
+
+    return graph
 
 
 if __name__ == "__main__":
