e101db561dac02d55a7346629392d62e6d2f3280
John Mercouris <john@mercouris.email>
2016-05-13 22:46:26 +0200

    Traversal work

--- source/visualize.py
+++ source/visualize.py
@@ -10,17 +10,14 @@ import graph
 pygame.init()
 
 # set up the window
-screen_width = 700
-screen_height = 400
+screen_width = 640
+screen_height = 480
 windowSurface = pygame.display.set_mode((screen_width, screen_height), 0, 32)
 pygame.display.set_caption('Network Simulation')
 
 # set up the colors
-BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 
 # draw the white background onto the surface
 windowSurface.fill(WHITE)
