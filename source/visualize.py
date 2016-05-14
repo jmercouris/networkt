0cddd89129c2e38d787c0a04915d1ee9eb9b51f5
John Mercouris <john@mercouris.email>
2016-05-14 15:35:58 +0200

    Encapsulated everything in main class

--- source/visualize.py
+++ source/visualize.py
@@ -2,41 +2,49 @@ import pygame
 from pygame.locals import *
 import sys
 import networkx as nx
-import pprint
 from graph.graph import load_graph_from_database
 
 
-# set up pygame
-pygame.init()
+def main():
+    # set up pygame
+    pygame.init()
 
-# set up the window
-screen_width = 640
-screen_height = 480
-windowSurface = pygame.display.set_mode((screen_width, screen_height), 0, 32)
-pygame.display.set_caption('Network Simulation')
+    # set up the window
+    screen_width = 640
+    screen_height = 480
+    windowSurface = pygame.display.set_mode((screen_width, screen_height), 0, 32)
+    pygame.display.set_caption('Network Simulation')
 
-# set up the colors
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
+    # set up the colors
+    WHITE = (255, 255, 255)
+    GREEN = (0, 255, 0)
 
-# draw the white background onto the surface
-windowSurface.fill(WHITE)
+    # draw the white background onto the surface
+    windowSurface.fill(WHITE)
+    
+    # generate a simple graph
+    graph = load_graph_from_database('FactoryBerlin')
+    layout = nx.spring_layout(graph)
+    
+    # draw the nodes
+    for node in layout:
+        pygame.draw.circle(windowSurface, GREEN,
+                           (int(layout[node][0] * 250) + 50, int(layout[node][1] * 250) + 50), 2, 0)
 
-# generate a simple graph
-graph = load_graph_from_database('FactoryBerlin')
-layout = nx.spring_layout(graph)
+    # draw the edges
+    for edge in graph.edges():
+        print(edge)
 
-for node in layout:
-    # print(node, 'corresponds to', (int(layout[node][0] * 250) + 50, int(layout[node][1] * 250) + 50))
-    pygame.draw.circle(windowSurface, GREEN,
-                       (int(layout[node][0] * 250) + 50, int(layout[node][1] * 250) + 50), 2, 0)
+        # draw the window onto the screen
+        pygame.display.update()
 
-# draw the window onto the screen
-pygame.display.update()
+    # run the game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
 
-# run the game loop
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+
+if __name__ == "__main__":
+    main()
