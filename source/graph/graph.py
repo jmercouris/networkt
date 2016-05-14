b72237c6266b9d3e83ad5ba9e043a543dbf16bc3
John Mercouris <jmercouris@gmail.com>
2016-05-14 16:13:11 +0200

    Working on visualizer

--- source/graph/graph.py
+++ source/graph/graph.py
@@ -16,64 +16,39 @@ session = DBSession()
 
 
 # Main Function
-def main(root_user='jmercouris'):
+def main(root_user='FactoryBerlin'):
     persist_graph(root_user, root_user)
 
 
 def persist_graph(screen_name, file_name):
-    # graph = load_graph_from_database(screen_name)
-    graph = nx.DiGraph()
-    root_user_object = session.query(Node).filter_by(screen_name=screen_name).first()
-    graph = traverse(root_user_object, 0, 10, {}, graph)
+    graph = load_graph_from_database(screen_name)
     nx.write_gml(graph, 'data/graph/' + file_name + '.gml')
 
 
 def load_graph_from_database(screen_name):
+    graph = nx.DiGraph()
     root_user_object = session.query(Node).filter_by(screen_name=screen_name).first()
-    graph = nx.Graph()
-    graph.add_node(root_user_object.screen_name, data=root_user_object.construct_dictionary())
-    
-    for node in root_user_object.reference_nodes():
-        graph.add_node(node.screen_name, data=node.construct_dictionary())
-        graph.add_edge(node.screen_name, root_user_object.screen_name)
-        for nodey in node.reference_nodes():
-            graph.add_node(nodey.screen_name, data=nodey.construct_dictionary())
-            graph.add_edge(nodey.screen_name, node.screen_name)
-        for nodey in node.pointer_nodes():
-            graph.add_node(nodey.screen_name, data=node.construct_dictionary())
-            graph.add_edge(node.screen_name, nodey.screen_name)
-
-    for node in root_user_object.pointer_nodes():
-        graph.add_node(node.screen_name, data=node.construct_dictionary())
-        graph.add_edge(root_user_object.screen_name, node.screen_name)
-        for nodey in node.reference_nodes():
-            graph.add_node(nodey.screen_name, data=nodey.construct_dictionary())
-            graph.add_edge(nodey.screen_name, node.screen_name)
-        for nodey in node.pointer_nodes():
-            graph.add_node(nodey.screen_name, data=node.construct_dictionary())
-            graph.add_edge(node.screen_name, nodey.screen_name)
-        
+    graph = traverse(root_user_object, 0, 1, {}, graph)
     return graph
 
 
 def traverse(node, depth, depth_limit, cache, graph):
     cache[node] = None
-    # print('__'*depth + node.name)
     if (depth > depth_limit):
         return graph
-    graph.add_node(node.screen_name, data=node.construct_dictionary())
+    graph.add_node(node.screen_name, node.construct_dictionary())
     for reference in node.pointer_nodes():
         graph.add_edge(node.screen_name, reference.screen_name)
         if reference in cache:
             continue
         graph = nx.compose(graph, traverse(reference, depth+1, depth_limit, cache, graph))
-
+    
     for reference in node.reference_nodes():
         graph.add_edge(reference.screen_name, node.screen_name)
         if reference in cache:
             continue
         graph = nx.compose(graph, traverse(reference, depth+1, depth_limit, cache, graph))
-
+    
     return graph
 
 
--- source/visualize.py
+++ source/visualize.py
@@ -8,36 +8,34 @@ from graph.graph import load_graph_from_database
 def main():
     # set up pygame
     pygame.init()
-
+    
     # set up the window
     screen_width = 640
     screen_height = 480
     windowSurface = pygame.display.set_mode((screen_width, screen_height), 0, 32)
     pygame.display.set_caption('Network Simulation')
-
+    
     # set up the colors
     WHITE = (255, 255, 255)
     GREEN = (0, 255, 0)
-
+    
     # draw the white background onto the surface
     windowSurface.fill(WHITE)
     
     # generate a simple graph
     graph = load_graph_from_database('FactoryBerlin')
+    print(graph.node['FactoryBerlin']['screenname'])
+    
     layout = nx.spring_layout(graph)
     
     # draw the nodes
     for node in layout:
         pygame.draw.circle(windowSurface, GREEN,
                            (int(layout[node][0] * 250) + 50, int(layout[node][1] * 250) + 50), 2, 0)
-
-    # draw the edges
-    for edge in graph.edges():
-        print(edge)
-
-        # draw the window onto the screen
-        pygame.display.update()
-
+    
+    # draw the window onto the screen
+    pygame.display.update()
+    
     # run the game loop
     while True:
         for event in pygame.event.get():
@@ -45,6 +43,15 @@ def main():
                 pygame.quit()
                 sys.exit()
 
+
+class Node(object):
+    """Class Representing a Node object, keeps track of position, edges, etc
+    
+    """
+    def __init__(self, args):
+        super(Node, self).__init__()
+        self.args = args
+        
 
 if __name__ == "__main__":
     main()
