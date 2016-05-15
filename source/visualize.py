818307e8e11bc347f9c54e335103d4858b84f88a
John Mercouris <john@mercouris.email>
2016-05-15 17:58:45 +0200

    Strange GUI added

--- source/visualize.py
+++ source/visualize.py
@@ -3,6 +3,11 @@ import pygame
 from pygame.locals import *
 import networkx as nx
 from graph.graph import load_graph_from_database
+from pgu import gui
+
+screen_width = 640
+screen_height = 480
+fps = 30
 
 
 def main():
@@ -10,9 +15,7 @@ def main():
     pygame.init()
     
     # set up the window
-    screen_width = 640
-    screen_height = 480
-    windowSurface = pygame.display.set_mode((screen_width, screen_height))
+    screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption('Network Simulation')
     
     # set up the colors
@@ -21,7 +24,7 @@ def main():
     GREEN = (0, 200, 0)
     
     # draw the white background onto the surface
-    windowSurface.fill(WHITE)
+    screen.fill(WHITE)
     
     # generate a simple graph
     graph = load_graph_from_database('FactoryBerlin')
@@ -29,31 +32,40 @@ def main():
     print(graph.node['FactoryBerlin']['screenname'])
     
     for edge in graph.edges():
-        pygame.draw.line(windowSurface, BLACK, true_position(layout[edge[0]]), true_position(layout[edge[1]]))
+        pygame.draw.line(screen, BLACK, true_position(layout[edge[0]]), true_position(layout[edge[1]]))
         print(layout[edge[0]])
     
     # draw the nodes
     for node in layout:
-        pygame.draw.circle(windowSurface, GREEN, true_position(layout[node]), 10, 0)
+        pygame.draw.circle(screen, GREEN, true_position(layout[node]), 10, 0)
         print(graph.node[node]['screenname'])
     
-    # button rectangle
-    pygame.draw.rect(windowSurface, GREEN, (50, 400, 30, 30))
+    form = gui.Form()
+    app = gui.App()
+    starCtrl = StarControl()
+    c = gui.Container(align=-1, valign=-1)
+    c.add(starCtrl, 0, 0)
+    app.init(c)
     
     # draw the window onto the screen
     pygame.display.update()
     
     # run the game loop
+    clock = pygame.time.Clock()
     running = True
     while running:
         for event in pygame.event.get():
-            print(pygame.mouse.get_pos())
             if event.type == QUIT:
                 running = False
-            if event.type == KEYDOWN and event.key == K_ESCAPE:
+            elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 running = False
-            if event.type == MOUSEBUTTONDOWN:
-                print(pygame.mouse.get_pos())
+            # elif event.type == MOUSEBUTTONDOWN:
+            #     print(pygame.mouse.get_pos())
+            else:
+                app.event(event)
+        clock.tick(fps) / 1000.0
+        app.paint()
+        pygame.display.flip()
     pygame.display.quit()
 
 
@@ -61,14 +73,54 @@ def true_position(coordinates):
     return (int(coordinates[0] * 250) + 50, int(coordinates[1] * 250) + 50)
 
 
-class Node(object):
-    """Class Representing a Node object, keeps track of position, edges, etc
-    
-    """
-    def __init__(self, args):
-        super(Node, self).__init__()
-        self.args = args
-        
+class StarControl(gui.Table):
+    def __init__(self, **params):
+        gui.Table.__init__(self, **params)
+
+        def fullscreen_changed(btn):
+            # pygame.display.toggle_fullscreen()
+            print("TOGGLE FULLSCREEN")
+
+        def stars_changed(slider):
+            print("Changed")
+
+        fg = (255, 255, 255)
+
+        self.tr()
+        self.td(gui.Label("Phil's Pygame GUI", color=fg), colspan=2)
+
+        self.tr()
+        self.td(gui.Label("Speed: ", color=fg), align=1)
+        e = gui.HSlider(100, -500, 500, size=20,
+                        width=100, height=16, name='speed')
+        self.td(e)
+
+        self.tr()
+        self.td(gui.Label("Size: ", color=fg), align=1)
+        e = gui.HSlider(2, 1, 5, size=20, width=100, height=16, name='size')
+        self.td(e)
+
+        self.tr()
+        self.td(gui.Label("Quantity: ", color=fg), align=1)
+        e = gui.HSlider(100, 1, 1000, size=20, width=100,
+                        height=16, name='quantity')
+        e.connect(gui.CHANGE, stars_changed, e)
+        self.td(e)
+
+        self.tr()
+        self.td(gui.Label("Color: ", color=fg), align=1)
+
+        btn = gui.Switch(value=False, name='fullscreen')
+        btn.connect(gui.CHANGE, fullscreen_changed, btn)
+
+        self.tr()
+        self.td(gui.Label("Full Screen: ", color=fg), align=1)
+        self.td(btn)
+
+        self.tr()
+        self.td(gui.Label("Warp Speed: ", color=fg), align=1)
+        self.td(gui.Switch(value=False, name='warp'))
+
 
 if __name__ == "__main__":
     main()
