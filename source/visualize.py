538f582482afa3b0d9c5945adfe645ca1995612a
John Mercouris <john@mercouris.email>
2016-05-15 18:14:24 +0200

    GUI Progress

--- source/visualize.py
+++ source/visualize.py
@@ -29,22 +29,20 @@ def main():
     # generate a simple graph
     graph = load_graph_from_database('FactoryBerlin')
     layout = nx.spring_layout(graph)
-    print(graph.node['FactoryBerlin']['screenname'])
     
     for edge in graph.edges():
         pygame.draw.line(screen, BLACK, true_position(layout[edge[0]]), true_position(layout[edge[1]]))
-        print(layout[edge[0]])
+        # print(layout[edge[0]])
     
     # draw the nodes
     for node in layout:
         pygame.draw.circle(screen, GREEN, true_position(layout[node]), 10, 0)
-        print(graph.node[node]['screenname'])
+        # print(graph.node[node]['screenname'])
     
-    form = gui.Form()
     app = gui.App()
-    starCtrl = StarControl()
+    root_control = RootControl()
     c = gui.Container(align=-1, valign=-1)
-    c.add(starCtrl, 0, 0)
+    c.add(root_control, 20, 440)
     app.init(c)
     
     # draw the window onto the screen
@@ -66,6 +64,7 @@ def main():
         clock.tick(fps) / 1000.0
         app.paint()
         pygame.display.flip()
+        pygame.display.update()
     pygame.display.quit()
 
 
@@ -73,53 +72,29 @@ def true_position(coordinates):
     return (int(coordinates[0] * 250) + 50, int(coordinates[1] * 250) + 50)
 
 
-class StarControl(gui.Table):
+class RootControl(gui.Table):
     def __init__(self, **params):
         gui.Table.__init__(self, **params)
 
         def fullscreen_changed(btn):
-            # pygame.display.toggle_fullscreen()
+            pygame.display.toggle_fullscreen()
             print("TOGGLE FULLSCREEN")
 
         def stars_changed(slider):
             print("Changed")
 
-        fg = (255, 255, 255)
+        fg = (0, 0, 0)
 
         self.tr()
-        self.td(gui.Label("Phil's Pygame GUI", color=fg), colspan=2)
 
-        self.tr()
         self.td(gui.Label("Speed: ", color=fg), align=1)
         e = gui.HSlider(100, -500, 500, size=20,
                         width=100, height=16, name='speed')
         self.td(e)
 
-        self.tr()
-        self.td(gui.Label("Size: ", color=fg), align=1)
-        e = gui.HSlider(2, 1, 5, size=20, width=100, height=16, name='size')
-        self.td(e)
-
-        self.tr()
-        self.td(gui.Label("Quantity: ", color=fg), align=1)
-        e = gui.HSlider(100, 1, 1000, size=20, width=100,
-                        height=16, name='quantity')
-        e.connect(gui.CHANGE, stars_changed, e)
-        self.td(e)
-
-        self.tr()
-        self.td(gui.Label("Color: ", color=fg), align=1)
-
-        btn = gui.Switch(value=False, name='fullscreen')
+        btn = gui.Button('Play')
         btn.connect(gui.CHANGE, fullscreen_changed, btn)
-
-        self.tr()
-        self.td(gui.Label("Full Screen: ", color=fg), align=1)
         self.td(btn)
-
-        self.tr()
-        self.td(gui.Label("Warp Speed: ", color=fg), align=1)
-        self.td(gui.Switch(value=False, name='warp'))
 
 
 if __name__ == "__main__":
