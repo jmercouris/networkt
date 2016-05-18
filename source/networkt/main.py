66258902c5d0ea16b33a6f7ac907e26257e03533
John Mercouris <jmercouris@gmail.com>
2016-05-18 16:36:27 +0200

    Layout appearing to be more logical

--- source/networkt/main.py
+++ source/networkt/main.py
@@ -1,9 +1,24 @@
+from random import random
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.graphics import Ellipse, Color, Line
 
 
 class NetworktUI(Widget):
     pass
+
+
+class Network(Widget):
+    def on_touch_down(self, touch):
+        with self.canvas:
+            color = (random(), random(), random())
+            Color(*color)
+            diameter = 30.
+            Ellipse(pos=(touch.x - diameter / 2, touch.y - diameter / 2), size=(diameter, diameter))
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
+    
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
 
 
 class NetworktApp(App):
