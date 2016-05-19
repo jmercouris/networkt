13fd50205d033ef55b5eaf02e5adf7e061d37df1
John Mercouris <john@mercouris.email>
2016-05-20 00:31:15 +0200

    Trying to implement separation of concerns

--- source/networkt/main.py
+++ source/networkt/main.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.graphics import Color, Ellipse
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.widget import Widget
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, DictProperty
 from kivy.clock import Clock
 import networkx as nx
 from graph.graph import load_graph_from_database, get_statuses_for_screen_name
@@ -11,18 +11,6 @@ from graph.graph import load_graph_from_database, get_statuses_for_screen_name
 class NetworktUI(Widget):
     def change_text(self):
         self.ids.messages.text = 'LOL'
-    
-    def load_graph(self):
-        # generate a simple graph
-        graph = load_graph_from_database('FactoryBerlin')
-        layout = nx.spring_layout(graph)
-        self.nodes = {}
-        # Generate the list of nodes with positions
-        for node in layout:
-            nodey = Node(graph.node[node])
-            nodey.position = Node.true_position(layout[node])
-            nodey.statuses = get_statuses_for_screen_name(nodey.screen_name)
-            self.nodes[nodey.screen_name] = nodey
     
     def update(self, dt):
         pass
@@ -33,6 +21,9 @@ class ScrollableLabel(ScrollView):
 
 
 class Network(Widget):
+    def __init__(self, **kwargs):
+        super(Network, self).__init__(**kwargs)
+
     def on_touch_down(self, touch):
         x = touch.x
         y = touch.y
@@ -41,16 +32,33 @@ class Network(Widget):
                 Color(0, 1, 0)
                 diameter = 30.
                 Ellipse(pos=(touch.x - diameter / 2, touch.y - diameter / 2), size=(diameter, diameter))
+                Ellipse(pos=(200, 200), size=(30.0, 30.0))
         else:
             return False
+    
+    def on_nodes(self, *args):
+        print('lol')
 
 
 class NetworktApp(App):
     def build(self):
         networktUI = NetworktUI()
-        networktUI.load_graph()
+        self.load_graph()
         Clock.schedule_interval(networktUI.update, 1.0 / 60.0)
         return networktUI
+    
+    nodes = DictProperty({})
+    
+    def load_graph(self):
+        # generate a simple graph
+        graph = load_graph_from_database('FactoryBerlin')
+        layout = nx.spring_layout(graph)
+        # Generate the list of nodes with positions
+        for node in layout:
+            nodei = Node(graph.node[node])
+            nodei.position = Node.true_position(layout[node])
+            nodei.statuses = get_statuses_for_screen_name(nodei.screen_name)
+            self.nodes[nodei.screen_name] = nodei
 
 
 class Node(object):
