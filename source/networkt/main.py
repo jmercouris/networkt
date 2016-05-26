f526491c893b5ce830411b08b8463220bacbc2a2
John Mercouris <jmercouris@gmail.com>
2016-05-26 12:39:02 +0200

    Showing sub player correctly

--- source/networkt/main.py
+++ source/networkt/main.py
@@ -47,13 +47,20 @@ class PreviewSlider(Slider):
         pass
     
     def update_graphic(self):
+        self.active_markers = []
+        for marker in self.markers:
+            if (marker.position * 100 > self.start and marker.position * 100 < self.end):
+                self.active_markers.append(marker)
+        total_difference = self.active_markers[-1].position - self.active_markers[0].position
+        for marker in self.active_markers:
+            marker.sub_position = abs(self.active_markers[0].position - marker.position) / total_difference
+        
         self.canvas.after.clear()
         with self.canvas.after:
-            for marker in self.markers:
-                if (marker.position * 100 > self.start and marker.position * 100 < self.end):
-                    Rectangle(size=(1, self.height),
-                              pos=(self.width * marker.position, self.pos[1]))
-            
+            for marker in self.active_markers:
+                Rectangle(size=(1, self.height),
+                          pos=(self.width * marker.sub_position, self.pos[1]))
+
 
 class PreviewRangeSlider(RangeSlider):
     nodes = DictProperty({})
