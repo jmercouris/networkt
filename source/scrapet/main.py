517d2bed4bdd2bc9e755733b52814abb65e60f00
John Mercouris <jmercouris@gmail.com>
2016-05-27 00:30:15 +0200

    Basic settings panel in place

--- source/scrapet/main.py
+++ source/scrapet/main.py
@@ -1,9 +1,24 @@
 from kivy.app import App
+from kivy.uix.settings import SettingsWithSidebar
+from scrapet.settings_panel import settings_json
 
 
 class ScrapetApp(App):
-    pass
-
+    def build(self):
+        self.settings_cls = SettingsWithSidebar
+    
+    def build_config(self, config):
+        config.setdefaults('example', {
+            'boolexample': True,
+            'numericexample': 10,
+            'optionsexample': 'option2',
+            'stringexample': 'some_string',
+            'pathexample': '~/'})
+    
+    def build_settings(self, settings):
+        settings.add_json_panel('Panel name',
+                                self.config,
+                                data=settings_json)
 
 if __name__ == '__main__':
     ScrapetApp().run()
