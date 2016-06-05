92166a90a5817c1c321475df224fa0e54dfa5e4a
John Mercouris <john@mercouris.email>
2016-06-06 00:46:24 +0200

    Node addition was somehow removed from code

--- source/graph/graph.py
+++ source/graph/graph.py
@@ -14,19 +14,19 @@ class Graph(object):
         DBSession = sessionmaker(bind=engine)
         self.session = DBSession()
     
+    def get_statuses_for_screen_name(self, screen_name):
+        element = self.session.query(Node).filter_by(screen_name=screen_name).first()
+        return element.statuses
+    
     def persist_graph(self, screen_name, graph_path, file_name):
         graph = self.load_graph_from_database(screen_name)
         nx.write_gml(graph, '{}/{}.gml'.format(graph_path, file_name))
     
-    def load_graph_from_database(self, screen_name):
+    def load_graph_from_database(self, screen_name, depth_limit=0):
         graph = nx.DiGraph()
         root_user_object = self.session.query(Node).filter_by(screen_name=screen_name).first()
-        graph = self.traverse(root_user_object, 0, 1, {}, graph)
+        graph = self.traverse(root_user_object, 0, depth_limit, {}, graph)
         return graph
-    
-    def get_statuses_for_screen_name(self, screen_name):
-        element = self.session.query(Node).filter_by(screen_name=screen_name).first()
-        return element.statuses
     
     def traverse(self, node, depth, depth_limit, cache, graph):
         cache[node] = None
@@ -34,12 +34,14 @@ class Graph(object):
             return graph
         graph.add_node(node.screen_name, node.construct_dictionary())
         for reference in node.pointer_nodes():
+            graph.add_node(reference.screen_name, reference.construct_dictionary())
             graph.add_edge(node.screen_name, reference.screen_name)
             if reference in cache:
                 continue
             graph = nx.compose(graph, self.traverse(reference, depth+1, depth_limit, cache, graph))
         
         for reference in node.reference_nodes():
+            graph.add_node(reference.screen_name, reference.construct_dictionary())
             graph.add_edge(reference.screen_name, node.screen_name)
             if reference in cache:
                 continue
