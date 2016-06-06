b2a15e07d9798b0f98ba8ed71e16e27be2fb3ecc
John Mercouris <jmercouris@gmail.com>
2016-06-06 15:24:45 +0200

    Broke apart network widget into separate file

--- source/networkt/main.py
+++ source/networkt/main.py
@@ -1,19 +1,14 @@
 import networkx as nx
 from kivy.app import App
 from kivy.clock import Clock
-from kivy.graphics import Color, Line, Rectangle
+from kivy.graphics import Rectangle, Color
 from kivy.properties import DictProperty, StringProperty, ObjectProperty, ListProperty, NumericProperty
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.widget import Widget
 from kivy.uix.slider import Slider
-from kivy.core.window import Window
-from kivy.uix.stencilview import StencilView
-from kivy.metrics import dp
-from math import pow
 from graph.graph import Graph
 from networkt.status import Status, by_date_key
 from networkt.node import Node
-from networkt.camera import Camera
 from networkt.range_slider import RangeSlider
 from kivy.factory import Factory
 
@@ -126,119 +121,10 @@ class Statuses(ScrollableLabel):
         self.text = statuses_string
 
 
-class Network(StencilView):
-    """Extends Stencilview to clip drawing to bounding box
-    
-    """
-    nodes = DictProperty({})
-    selected_node = ObjectProperty(None)
-    time = NumericProperty(0)
-    
-    def __init__(self, **kwargs):
-        super(Network, self).__init__(**kwargs)
-        self._keyboard = Window.request_keyboard(self._keyboard_closed, self, 'text')
-        self._keyboard.bind(on_key_down=self._on_keyboard_down)
-        self.camera = Camera()
-    
-    def _keyboard_closed(self):
-        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
-        self._keyboard = None
-        
-    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        if (text == 'w'):
-            self.camera.shift_down()
-            self.update_node_positions()
-            return True
-        elif (text == 'a'):
-            self.camera.shift_right()
-            self.update_node_positions()
-            return True
-        elif (text == 's'):
-            self.camera.shift_up()
-            self.update_node_positions()
-            return True
-        elif (text == 'd'):
-            self.camera.shift_left()
-            self.update_node_positions()
-            return True
-        elif (text == 'e'):
-            self.camera.zoom_in()
-            self.update_node_positions()
-            return True
-        elif (text == 'q'):
-            self.camera.zoom_out()
-            self.update_node_positions()
-            return True
-        
-        return False
-    
-    def on_touch_down(self, touch):
-        # Handle Mouse Zoom
-        if touch.button is 'scrollup' and self.collide_point(*touch.pos):
-            self.camera.zoom_out()
-            self.update_node_positions()
-            return True
-        if touch.button is 'scrolldown' and self.collide_point(*touch.pos):
-            self.camera.zoom_in()
-
-            self.update_node_positions()
-            return True
-    
-    def on_touch_move(self, touch):
-        # Handle Mouse Drag
-        if self.collide_point(*touch.opos):
-            self.camera.shift_offset((touch.dpos[0], touch.dpos[1]))
-            self.update_node_positions()
-    
-    def on_touch_up(self, touch):
-        # Handle Clicks Within Nodes
-        if touch.button is 'left':
-            for node in self.nodes:
-                nodei = self.nodes[node]
-                # (R0-R1)^2 <= (x0-x1)^2+(y0-y1)^2 <= (R0+R1)^2
-                squared_x = pow((touch.x - nodei.render_position[0]), 2)
-                squared_y = pow((touch.y - nodei.render_position[1]), 2)
-                squared_radius = pow(nodei.radius, 2)
-                if (dp(squared_radius) > squared_x + squared_y):
-                    self.selected_node = nodei
-    
-    def update_node_positions(self):
-        for node in self.nodes:
-            nodei = self.nodes[node]
-            nodei.render_position = self.translate_render(nodei.position)
-        self.update_graphic()
-    
-    def translate_render(self, position):
-        position = (int(position[0] * self.camera.zoom) + 50, int(position[1] * self.camera.zoom) + 200)
-        position = (position[0] + self.camera.position[0], position[1] + self.camera.position[1])
-        position = (dp(position[0]), dp(position[1]))
-        return position
-    
-    def update_graphic(self):
-        self.canvas.clear()
-        with self.canvas:
-            Color(0, 1, 0)
-            for node in self.nodes:
-                nodei = self.nodes[node]
-                # Draw All Nodes
-                Line(circle=(nodei.render_position[0], nodei.render_position[1], dp(nodei.radius)))
-                # Draw All Edges
-                for edge in nodei.edges:
-                    Line(points=(nodei.render_position[0], nodei.render_position[1],
-                                 edge.render_position[0], edge.render_position[1]))
-                # Draw All Statuses
-                for status in nodei.active_statuses:
-                    Line(circle=(status.render_position[0], status.render_position[1], dp(status.radius)))
-    
-    def update_logic(self):
-        for node in self.nodes:
-            nodei = self.nodes[node]
-            nodei.act()
-
-
 class NetworktApp(App):
     nodes = DictProperty({})
     Factory.register('RangeSlider', module='networkt.range_slider')
+    Factory.register('Network', module='networkt.network')
     
     def build(self):
         self.networktUI = NetworktUI()
