a3b5bbd69d548c0b0a04f306c3819169047e9bfe
John Mercouris <john@mercouris.email>
2016-06-07 22:24:14 +0200

    Working on preview slider

--- source/networkt/preview_slider.py
+++ source/networkt/preview_slider.py
@@ -8,30 +8,45 @@ class PreviewSlider(Slider):
     end = NumericProperty(100)
     markers = ListProperty([])
     
-    def on_start(self, *args):
+    def on_markers(self, *args):
         self.update_graphic()
     
+    def on_start(self, *args):
+        self.update_logic()
+    
     def on_end(self, *args):
-        self.update_graphic()
+        self.update_logic()
     
     def update_logic(self):
         pass
     
     def on_width(self, *args):
-        pass
+        self.update_object_positions()
     
     def update_graphic(self):
-        self.active_markers = []
-        for marker in self.markers:
-            if (marker.position * 100 > self.start and marker.position * 100 < self.end):
-                self.active_markers.append(marker)
-        total_difference = self.active_markers[-1].position - self.active_markers[0].position
-        for marker in self.active_markers:
-            marker.sub_position = abs(self.active_markers[0].position - marker.position) / total_difference
+        # self.active_markers = []
+        # for marker in self.markers:
+        #     if (marker.position * 100 > self.start and marker.position * 100 < self.end):
+        #         self.active_markers.append(marker)
+        # total_difference = self.active_markers[-1].position - self.active_markers[0].position
+        # for marker in self.active_markers:
+        #     marker.sub_position = abs(self.active_markers[0].position - marker.position) / total_difference
         
-        self.canvas.after.clear()
-        with self.canvas.after:
-            Color(0, 1, 0, .5)
-            for marker in self.active_markers:
-                Rectangle(size=(self.width / len(self.active_markers), self.height),
-                          pos=(self.width * marker.sub_position, self.pos[1]))
+        # self.canvas.after.clear()
+        # with self.canvas.after:
+        #     Color(0, 1, 0, .5)
+        #     for marker in self.active_markers:
+        #         Rectangle(size=(self.width / len(self.active_markers), self.height),
+        #                   pos=(self.width * marker.sub_position, self.pos[1]))
+        pass
+    
+    def update_object_positions(self):
+        pass
+    
+    # def update_graphic(self):
+    #     self.canvas.before.clear()
+    #     for marker in self.markers:
+    #         instruction_group = InstructionGroup()
+    #         instruction_group.add(Color(0, 1, 0, marker.opacity))
+    #         instruction_group.add(marker.representation)
+    #         self.canvas.before.add(instruction_group)
