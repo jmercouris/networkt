a65a92e598b92893f48584b3f4fb54cfafe2bc46
John Mercouris <jmercouris@gmail.com>
2016-06-14 00:01:17 +0200

    Slices discrete timess

--- source/networkt/preview_slider.py
+++ source/networkt/preview_slider.py
@@ -49,9 +49,8 @@ class PreviewSlider(Slider):
         if (self.value >= self.max):
             self.value = self.min
         # Calculate Time Range to Query Network Data Structure
-        self.time_slice_start = (self.time_resolution * self.value) + self.time_start
-        self.time_slice_end = self.time_slice_start + self.time_resolution
-        print(self.time_slice_start, self.time_slice_end)
+        self.time_slice_start = int((self.time_resolution * self.value) + self.time_start)
+        self.time_slice_end = int(self.time_slice_start + (self.time_resolution * self.step_resolution))
     
     def update_logic(self):
         self.active_markers = []
