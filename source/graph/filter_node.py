9db801b56839fbd0fcd0f7055264c7f44bb9e317
John Mercouris <jmercouris@gmail.com>
2016-06-22 01:04:52 +0200

    Logger doesnt crash as easily

--- source/graph/filter_node.py
+++ source/graph/filter_node.py
@@ -18,12 +18,21 @@ def logical_distribution(node):
     for time_zone in [n.time_zone for n in node.reference_nodes()]:
         if (time_zone is not None):
             time_zone_list.append(time_zone)
+    
+    # If they dont have any reference nodes with time zones, they cannot be quantified
+    if(len(time_zone_list) < 1):
+        return 0
+    
     counts = Counter(time_zone_list).most_common(2)
     inclusive_count = 0
     for key in counts:
         inclusive_count = inclusive_count + key[1]
         
-    return ((inclusive_count > len(time_zone_list) * .50) and (abs(counts[0][1] - counts[1][1]) / counts[0][1] < .8))
+    try:
+        return ((inclusive_count > len(time_zone_list) * .50) and
+                (abs(counts[0][1] - counts[1][1]) / counts[0][1] < .8))
+    except:
+        return 0
 
 
 def logical_time_zone(node):
