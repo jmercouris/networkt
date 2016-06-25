53be4b88caa4b836e15478a8156a22f5a4024854
John Mercouris <jmercouris@gmail.com>
2016-06-25 15:11:28 +0200

    Clustering, attempt to use real articles

--- source/cluster/program.py
+++ source/cluster/program.py
@@ -1,14 +1,15 @@
-import string
-import collections
 from configparser import ConfigParser
 from graph.graph import Graph
+import os
+
+import string
+import collections
 from nltk import word_tokenize
-from nltk import FreqDist
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
-import os
+from pprint import pprint
 
 
 def process_text(text, stem=True):
@@ -46,41 +47,29 @@ def cluster_texts(texts, clusters=3):
  
  
 if __name__ == "__main__":
-    config = ConfigParser()
-    config.read(os.path.expanduser('~/.config/networkt/cluster.ini'))
-    DATABASE_NAME = 'sqlite:///{}/data_store.db'.format(config.get('persistence-configuration', 'database_path'))
+    # config = ConfigParser()
+    # config.read(os.path.expanduser('~/.config/networkt/cluster.ini'))
+    # DATABASE_NAME = 'sqlite:///{}/data_store.db'.format(config.get('persistence-configuration', 'database_path'))
     
-    graph = Graph(DATABASE_NAME)
-    statuses = graph.load_statuses()
-    articles = []
-    count = 0
-    for status in statuses:
-        count = count + 1
-        articles.append(status.text)
-    print(count)
+    # graph = Graph(DATABASE_NAME)
+    # statuses = graph.load_statuses()
+    # articles = []
+    # count = 0
+    # for status in statuses:
+    #     articles.append(status.text)
 
-    # articles = ['article about stuff',
-    #             'another cool article',
-    #             'this is what articles are made of',
-    #             'another cool article',
-    #             'another cool article lol']
+    articles = ['article about stuff',
+                'another cool article',
+                'this is what articles are made of',
+                'another cool article',
+                'article about stuff',
+                'another cool article',
+                'this is what articles are made of',
+                'another cool article lol']
 
-    # clusters = cluster_texts(articles, 20)
-    # clusteri = dict(clusters)
-    
-    # for key in clusteri:
-    #     print('Key: ', key)
-    #     key_articles_index = list(clusteri[key])
-        
-    #     cluster_data = ''
-    #     for index in key_articles_index:
-    #         cluster_data += articles[index]
-        
-    #     # Only Non Stop Words
-    #     stop_words = set(stopwords.words('english'))
-    #     cluster_data = [word for word in cluster_data if word not in stop_words]
-        
-    #     freq = FreqDist(cluster_data)
-    #     print(list(freq.items())[:10])
-
+    clusters = cluster_texts(articles, 4)
+    clusteri = dict(clusters)
+    for key in clusteri:
+        print('idx {} cmp {}'.format(key, len(clusteri[key])))
+    pprint(clusteri)
 
