fd9b286964b8634209646cbc4fe1acd7b94b9a99
John Mercouris <john@mercouris.email>
2016-07-30 21:51:52 +0200

    Added info org file for organization, add mds for visualization of k means clusters

--- source/cluster/program3.py
+++ source/cluster/program3.py
@@ -11,6 +11,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from graph.initialize import Base, Node
+from sklearn.metrics.pairwise import cosine_similarity
+import matplotlib.pyplot as plt
+from sklearn.manifold import MDS
+import pandas as pd
 
 
 def process_text(text, stem=True):
@@ -40,6 +44,15 @@ def cluster_texts(texts, clusters=3):
     km_model.fit(tfidf_model)
  
     clustering = collections.defaultdict(list)
+    dist = 1 - cosine_similarity(tfidf_model)
+
+    mds = MDS(n_components=2, dissimilarity="precomputed", random_state=1)
+    pos = mds.fit_transform(dist)  # shape (n_components, n_samples)
+    xs, ys = pos[:, 0], pos[:, 1]
+    
+    plt.plot(xs, ys, 'go')
+    
+    plt.show()
  
     for idx, label in enumerate(km_model.labels_):
         clustering[label].append(idx)
@@ -50,7 +63,7 @@ def cluster_texts(texts, clusters=3):
 def create_session():
     config = ConfigParser()
     config.read(os.path.expanduser('~/.config/networkt/cluster.ini'))
-    DATABASE_NAME = 'sqlite:///{}/data_store.db'.format(config.get('persistence-configuration', 'database_path'))
+    DATABASE_NAME = 'sqlite:///{}/data_store_backup.db'.format(config.get('persistence-configuration', 'database_path'))
     engine = create_engine(DATABASE_NAME)
     Base.metadata.bind = engine
     DBSession = sessionmaker(bind=engine)
@@ -65,15 +78,15 @@ if __name__ == "__main__":
         print('User ', user.screen_name)
         statuses = []
         statuses = statuses + user.statuses
-        for node in user.reference_nodes():
+        for node in user.reference_nodes()[0:1]:
             statuses = statuses + node.statuses
-        for node in user.pointer_nodes():
+        for node in user.pointer_nodes()[0:1]:
             statuses = statuses + node.statuses
         
         statuses.sort()
         documents = [i.text for i in statuses]
         print('Documents Gathered')
-        cluster_count = 500
+        cluster_count = 20
         clusters = cluster_texts(documents, cluster_count)
         clusteri = dict(clusters)
         for key in clusteri:
