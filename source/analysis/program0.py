39fd9a0f2f627335641fee7e4818585a4c22b859
John Mercouris <john@mercouris.email>
2016-09-18 22:14:55 -0500

    Analytics program to gather data about users

---
+++ source/analysis/program0.py
@@ -0,0 +1,32 @@
+# Count the Friends and Followers of each Transnational Entrepreneur
+import os
+from configparser import ConfigParser
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from graph.initialize import Base, Node
+
+
+def create_session():
+    config = ConfigParser()
+    config.read(os.path.expanduser('~/.config/networkt/cluster.ini'))
+    DATABASE_NAME = 'sqlite:///{}/data_store.db'.format(config.get('persistence-configuration', 'database_path'))
+    engine = create_engine(DATABASE_NAME)
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    return session
+
+
+if __name__ == "__main__":
+    session = create_session()
+    transnational_users = session.query(Node).filter_by(filter_1=True).all()
+    for user in transnational_users:
+        print('User ', user.screen_name)
+        # statuses = []
+        # statuses = statuses + user.statuses
+        # for node in user.reference_nodes():
+        #     statuses = statuses + node.statuses
+        # for node in user.pointer_nodes():
+        #     statuses = statuses + node.statuses
+        
+
