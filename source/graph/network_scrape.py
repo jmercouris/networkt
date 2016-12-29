ba06abaf5c82d113a480f46a64bb0d4ea471799a
John Mercouris <john@mercouris.email>
2016-12-29 01:38:59 -0600

    Changed entire codebase to use graph database

--- source/graph/network_scrape.py
+++ source/graph/network_scrape.py
@@ -1,7 +1,7 @@
 import time
 from math import ceil as ceiling
 from twython import Twython
-from graph.data_model import Node
+from graph.data_model import Node, Status
 import neomodel
 
 
@@ -21,25 +21,27 @@ class NetworkScrape(object):
         except neomodel.exception.UniqueProperty:
             # user already exists, retrieve
             return Node.nodes.get(screen_name=screen_name)
+        except Exception as e:
+            print('System failure: {}'.format(e))
     
-    def pull_follow_network(self, user_object, limit):
+    def pull_follow_network(self, user, limit):
         scope_limit = ceiling(limit / self.scope_depth)
         
-        self.pull_remote_graph(user_object, user_object.followers,
+        self.pull_remote_graph(user, user.followers,
                                scope_limit, self.twitter.get_followers_list)
     
-    def pull_friend_network(self, user_object, limit):
+    def pull_friend_network(self, user, limit):
         scope_limit = ceiling(limit / self.scope_depth)
         
-        self.pull_remote_graph(user_object, user_object.friends,
+        self.pull_remote_graph(user, user.friends,
                                scope_limit, self.twitter.get_friends_list)
     
-    def pull_remote_graph(self, user_object, relationship,
+    def pull_remote_graph(self, user, relationship,
                           scope_limit, twitter_function):
         next_cursor = -1
         while(next_cursor and scope_limit):
             scope_limit -= 1
-            search = twitter_function(screen_name=user_object.screen_name,
+            search = twitter_function(screen_name=user.screen_name,
                                       count=self.scope_depth, cursor=next_cursor)
             for result in search['users']:
                 tmp = None
@@ -48,26 +50,24 @@ class NetworkScrape(object):
                 except neomodel.exception.UniqueProperty:
                     # user already exists, retrieve
                     tmp = Node.nodes.get(screen_name=result['screen_name'])
+                except Exception as e:
+                    print('System failure: {}'.format(e))
                 relationship.connect(tmp)
             next_cursor = search["next_cursor"]
             time.sleep(60)
     
-    # def pull_remote_status(self, screen_name, scope_depth=200):
-    #     user_object = self.session.query(Node).filter_by(screen_name=screen_name).first()
-    #     if (user_object is None and len(user_object.statuses) > 0):
-    #         return
+    def pull_remote_status(self, user, scope_depth=200):
+        # if we have already retrieved statuses, or they have no statuses, return
+        if len(user.statuses) > 0 or user.statuses_count <= 0:
+            return
         
-    #     try:
-    #         # We have recorded 0 Statuses previously for this user, therefore we can reasonably assume
-    #         # An object of the same credentials does not exist in the database, also it is within a try/catch
-    #         statuses = self.twitter.get_user_timeline(screen_name=screen_name, count=scope_depth)
-    #         for status in statuses:
-    #             user_object.statuses.append(Status(status))
-    #     except:
-    #         pass
+        statuses = self.twitter.get_user_timeline(screen_name=user.screen_name, count=scope_depth)
+        for status in statuses:
+            try:
+                user.statuses.connect(Status.create_from_response(status))
+            except neomodel.exception.UniqueProperty:
+                print('Error: status already exists in database')
+            except Exception as e:
+                print('System failure: {}'.format(e))
         
-    #     self.session.commit()
-    #     time.sleep(7)
-    
-
-
+        time.sleep(5)
